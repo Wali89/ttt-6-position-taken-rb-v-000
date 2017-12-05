2226aebb2a57b021910a99c8e6f7836ff69957e6
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-05 12:56:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 def position_taken?(board, index)
-  if (board[index] == "X" || "O")
-    true
-  if (board[index] == " " || "")
-    false
-  else
-    false
-  end
-end
+  taken = nil
+ if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+ taken = false
+ else
+ taken = true
+ end
+ taken
